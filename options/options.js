aa406aaf0529e88353dc7db010f21d38e77004d7
Crowdbotics <team@crowdbotics.com>
2024-05-22 17:05:33 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dark-lab-48062",
+  url: "https://dark-lab-48062.botics.co",
+  api: "https://dark-lab-48062.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
